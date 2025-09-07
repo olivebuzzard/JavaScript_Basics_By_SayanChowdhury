bf5ee48567187e3ec383107ae78296cebfe83e49
Sayan Chowdhury <csayan818@gmail.com>
2025-09-07 04:55:37 +0000

    datatypes done

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict" //treat all js coder as new version
+
+// alert( 3 + 3) we are using node.js instead of browser
+
+console.log(" 3 + 3")// code reliability should be high
+
+console.log("Sayan")
+
+let name = "Sayan"
+let age = 18
+let isLoggedin = false
+
+//numbers 2 to the power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined 
+// symbol=> unique
+
+
+//object
+
+console.log(typeof undefined) //for undefined structure
+console.log(typeof null)//object
